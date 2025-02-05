cadfec3a4c1ab9312da7b38dd33466136752d3c6
GGvibes <grherzog@gmail.com>
2025-02-04 20:44:23 -0600

    get, delete

--- index.js
+++ index.js
@@ -2,35 +2,84 @@ const express = require("express");
 const pg = require("pg");
 
 const client = new pg.Client(
-    process.env.DATABASE_URL || 
+  process.env.DATABASE_URL ||
     "postgres://postgres:juniper23@localhost/acme_ice_cream"
-)
+);
 
 const app = express();
 
 app.use(express.json());
 app.use(require("morgan")("dev"));
 
-
+//GET all flavors
+app.get("/api/flavors", async (req, res, next) => {
+  try {
+    const SQL = `SELECT * FROM flavors ORDER BY created_at DESC`;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+//GET single flavor
+// app.get("/api/flavors/:id", async (req, res, next) => {});
+//POST new flavor
+app.post("/api/flavors", async (req, res, next) => {
+  try {
+    console.log("POST request recieved", req.body);
+    const SQL = `INSERT INTO flavors (name, is_favorite, created_at=now()) VALUES ($1, $2, $3) RETURNING *`;
+    const response = await client.query(SQL, req.body.name);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+//DELETE flavor
+app.delete("/api/flavors/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+    DELETE from flavors
+    WHERE id=$1;
+    `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+//PUT update flavor
+app.put("/api/flavors/:id", async (req, res, next) => {
+  try {
+    console.log("PUT request received for ID:", req.params.id);
+    const SQL = `
+      UPDATE flavors
+      SET name=$1, is_favorite=$2, updated_at=now()
+      WHERE id = $3 RETURNING *;
+      `;
+  } catch (error) {
+    next(error);
+  }
+});
 const init = async () => {
-    await client.connect();
-    console.log("connected to db");
-    let SQL = `
+  await client.connect();
+  console.log("connected to db");
+  let SQL = `
         DROP TABLE IF EXISTS flavors;
         CREATE TABLE flavors (
             id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            is_favorite BOOLEAN,
             created_at TIMESTAMP DEFAULT now(),
-            updated_at TIMESTAMP DEFAULT now(),
-            txt VARCHAR(255) NOT NULL
+            updated_at TIMESTAMP DEFAULT now()
             );
-        INSERT INTO flavors(txt) VALUES('Strawberry');
-        INSERT INTO flavors(txt) VALUES('Superman');
-        INSERT INTO flavors(txt) VALUES('Chocolate Brownie Fudge Chunk');
+        INSERT INTO flavors(name, is_favorite) VALUES('Strawberry', false);
+        INSERT INTO flavors(name, is_favorite) VALUES('Superman', false);
+        INSERT INTO flavors(name, is_favorite) VALUES('Chocolate Brownie Fudge Chunk', true);
         `;
-    await client.query(SQL)
-    console.log("tables created and data seeded");
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`listening on port ${port}`));
-}
+  await client.query(SQL);
+  console.log("tables created and data seeded");
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
 
 init();
